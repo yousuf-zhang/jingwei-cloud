ca0ff6d4d7561752704205d90fe36a83787cf164
yousuf <marui801234>
2020-11-13 18:02:55 +0800

    feat: sparrow模块
    搭建企业级的gradle项目管理模块 sparrow

---
+++ sparrow/sparrow-gradle-plugin/src/main/java/com/vastmoon/sparrow/gradle/SparrowGradlePlugin.java
@@ -0,0 +1,19 @@
+package com.vastmoon.sparrow.gradle;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+import org.springframework.boot.gradle.plugin.SpringBootPlugin;
+
+/**
+ * <p> ClassName: SparrowGradlePlugin
+ * <p> Description: 插件
+ *
+ * @author yousuf 2020/11/11
+ */
+public class SparrowGradlePlugin implements Plugin<Project> {
+    public static final String BOM_COORDINATES = "com.vastmoon.sparrow:sparrow-dependencies:";
+    @Override
+    public void apply(Project project) {
+        project.getPlugins().apply(SpringBootPlugin.class);
+    }
+}
